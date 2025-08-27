c09185af10d41ea50a13655881eb74f881b13e5f
Mohan Kumar <115875676+mohan03366@users.noreply.github.com>
2025-08-27 12:50:15 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -13,7 +13,7 @@ connectDB();
 // Middleware
 app.use(
   cors({
-    origin: "https://job-portals-3.onrender.com", // your frontend Render URL
+    origin: "https://job-portals-4.onrender.com", // your frontend Render URL
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], // allow CRUD + PATCH + OPTIONS
     allowedHeaders: ["Content-Type", "Authorization"], // allow headers for JSON & JWT
